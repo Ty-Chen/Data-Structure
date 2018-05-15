1377589b2169463d23e6baa4dfcfe4f10772e524
ty <tianyuch@hotmail.com>
2018-05-15 07:16:10 -0700

    change main function to test offsetof

--- main.c
+++ main.c
@@ -2,10 +2,30 @@
 #include <stdlib.h>
 #include "list.h"
 
+typedef struct data_list
+{
+	int  test_int_data;
+	char test_char_data; 
+	struct list_head head;
+}data_list;
+
+
 int main()
 {
-	LIST_HEAD(list);
-	printf("complete a simple list\n");
+	//LIST_HEAD(list);
+	data_list dataHead;
+	dataHead.test_int_data = 123;
+	dataHead.test_char_data = 'a';
+
+	/* We test offsetof here*/
+	printf("Firstly, we test the offset marco\n");
+	printf("offset of test_int_data = %lu\n", offsetof(data_list, test_int_data));
+	printf("offset of test_char_data = %lu\n", offsetof(data_list, test_char_data));
+	printf("offset of list_head = %lu\n", offsetof(data_list, head));
+
+	/**/
+	
+	printf("\ncomplete a simple list\n");
 	/* We test list here*/
 	
 	//getchar();
