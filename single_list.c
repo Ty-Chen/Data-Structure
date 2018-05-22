fd6513902f6c38ba405bd9aa5326918fb00bf146
Ty-Chen <tianyuch@hotmail.com>
2018-05-22 22:02:55 +0800

    Update single_list.c

--- single_list.c
+++ single_list.c
@@ -7,9 +7,14 @@ typedef sing_list s_list
 	sing_list *next;
 }s_list;
 
-s_list *add_node(s_list *node)
+s_list *add_node(s_list *loc, s_list *node)
 {
-	
+	if (node != NULL)
+	{
+		node->next = loc->next;
+		loc->next = node->next;		
+	}
+
 }
 
 s_list *del_node(s_list *node)
