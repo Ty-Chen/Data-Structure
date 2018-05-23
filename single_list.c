312100f77feb3bff5b5f3845685fb2f3bee25535
ty <tianyuch@hotmail.com>
2018-05-23 08:52:27 -0700

    sb

--- single_list.c
+++ single_list.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef sing_list s_list
+typedef struct s_list
 {
 	int val;
-	sing_list *next;
+	s_list *next;
 }s_list;
 
 s_list *add_node(s_list *loc, s_list *node)
@@ -12,14 +12,22 @@ s_list *add_node(s_list *loc, s_list *node)
 	if (node != NULL)
 	{
 		node->next = loc->next;
-		loc->next = node->next;		
+		loc->next = node;		
 	}
 
 }
 
 s_list *del_node(s_list *node)
 {
-	
+	if (node != NULL && node->next != NULL)
+	{
+		s_list *temp = node->next;
+		node->next = temp->next;
+		node->val = temp->val;
+		temp = NULL;
+	}
+	else
+		node = NULL;
 }
 
 int main()
