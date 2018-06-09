693742c53c4d253f950cd5cd0c12e2b10b1d1fb1
ty <tianyuch@hotmail.com>
2018-06-09 05:51:31 -0700

    Change rbt

--- rbt.c
+++ rbt.c
@@ -1,8 +1,26 @@
 /*
  * This file implements red black tree structure and functions
  */
+#include "stdio.h"
+#include "stdlib.h"
+
+typedef enum ColorType {RED, BLACK} ColorType;
 
 typedef struct rbnode{
-
-
+	int key;
+	rbnode *left;
+	rbnode *right;
+	rbnode *p;
+	ColorType color;
 }rbnode;
+
+typedef struct rb_root{
+	rbnode *root;
+	rbnode *nil;
+}rb_root;
+
+rb_root *rbt_init()
+{
+	rb_root *T;
+}
+	
