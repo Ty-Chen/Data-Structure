72a8dfdc1e8a6a562f95f29d8d740550ada26fcf
ty <tianyuch@hotmail.com>
2018-06-10 07:34:03 -0700

    change red black tree

--- rbt.c
+++ rbt.c
@@ -22,5 +22,17 @@ typedef struct rb_root{
 rb_root *rbt_init()
 {
 	rb_root *T;
+	T = (rb_root*)malloc(sizeof(rb_root));
+    assert( NULL != T);
+
+    //用一个哨兵代表NIL。
+    T->nil = (rbnode*)malloc(sizeof(rbnode));
+    assert(NULL != T->nil);
+    T->nil->color = BLACK;
+    T->nil->left = T->nil->right = NULL;
+    T->nil->p = NULL;
+
+    T->root = T->nil;
+    return T;
 }
 	
