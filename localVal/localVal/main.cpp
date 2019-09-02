f3d2ea1e9ed3e7ccf30ffdb64743f6d23477786f
Ty-Chen <369494003@qq.com>
2019-09-02 20:50:27 +0800

    Update some test functions

--- localVal/localVal/main.cpp
+++ localVal/localVal/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <windows.h>
 
+#define TEST_COUNT 3000000000
+
 int global = 0;
 static int staticVal = 0;
 
@@ -10,56 +12,67 @@ public:
 	int m_val = 0;
 };
 
-int test1()
+void test1(Test *t)
 {
-	int tmp = 0;
-	for (int i = 0; i < 1000000000; i++)
-	{
-		tmp += i;
-	}
-	return tmp;
-}
-
-void test2(Test *t)
-{
-	for (int i = 0; i < 1000000000; i++)
+	for (int i = 0; i < TEST_COUNT; i++)
 	{
 		t->m_val += i;
 	}
 	return;
 }
 
-int test3()
+int test2()
 {
-	for (int i = 0; i < 1000000000; i++)
+	for (int i = 0; i < TEST_COUNT; i++)
 	{
 		global += i;
 	}
 	return global;
 }
 
-int test4()
+int test3()
 {
-	for (int i = 0; i < 1000000000; i++)
+	for (int i = 0; i < TEST_COUNT; i++)
 	{
 		staticVal += i;
 	}
 	return staticVal;
 }
 
+int test4()
+{
+	int tmp = 0;
+	for (int i = 0; i < TEST_COUNT; i++)
+	{
+		tmp += i;
+	}
+	return tmp;
+}
+
+int test5(int val)
+{
+	for (int i = 0; i < TEST_COUNT; i++)
+	{
+		val += i;
+	}
+	return val;
+}
+
 int main()
 {
 	Test test;
 	unsigned long now, last;
+
+	test.m_val = 0;
 	now = last = GetTickCount();
-	test.m_val = test1();
+	test1(&test);
 	now = GetTickCount();
-	std::cout << "test 1 val = " << test.m_val << ", now =  " << now 
+	std::cout << "test 1 val = " << test.m_val << ", now =  " << now
 		<< ", last = " << last << ", time cost = " << now - last << std::endl;
 
 	test.m_val = 0;
 	now = last = GetTickCount();
-	test2(&test);
+	test.m_val = test2();
 	now = GetTickCount();
 	std::cout << "test 2 val = " << test.m_val << ", now =  " << now
 		<< ", last = " << last << ", time cost = " << now - last << std::endl;
@@ -71,11 +84,26 @@ int main()
 	std::cout << "test 3 val = " << test.m_val << ", now =  " << now
 		<< ", last = " << last << ", time cost = " << now - last << std::endl;
 
-	test.m_val = 0;
 	now = last = GetTickCount();
 	test.m_val = test4();
 	now = GetTickCount();
 	std::cout << "test 4 val = " << test.m_val << ", now =  " << now
+		<< ", last = " << last << ", time cost = " << now - last << std::endl;
+
+	test.m_val = 0;
+	int tmp = 0;
+	now = last = GetTickCount();
+	test.m_val = test5(tmp);
+	now = GetTickCount();
+	std::cout << "test 5 val = " << test.m_val << ", now =  " << now
+		<< ", last = " << last << ", time cost = " << now - last << std::endl;
+
+	test.m_val = 0;
+	now = last = GetTickCount();
+	tmp = 0;
+	test.m_val = test5(tmp);
+	now = GetTickCount();
+	std::cout << "test 6 val = " << test.m_val << ", now =  " << now
 		<< ", last = " << last << ", time cost = " << now - last << std::endl;
 
 	getchar();
